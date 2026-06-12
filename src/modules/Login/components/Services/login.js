a57f1d83e94fff58435912e6e8cd68b94b06e5d7
SRRN2025 <sasha.ramirez24903529@estu.unan.edu.ni>
2026-06-11 22:42:24 -0600

    Implementacion de login con authentication API

---
+++ src/modules/Login/components/Services/login.js
@@ -0,0 +1,47 @@
+const API_BASE = "https://localhost:7249/api/Auth";
+
+export async function login(userName, password) {
+  const res = await fetch(`${API_BASE}/Auth`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      userName,
+      password,
+    }),
+  });
+
+  if (!res.ok) {
+    const err = await res.json();
+    const msgs = err.Errors?.join("\n") ?? "Usuario o contraseña incorrectos";
+    throw new Error(msgs);
+  }
+
+  const json = await res.json();
+
+  localStorage.setItem("user", JSON.stringify(json));
+
+  return json;
+}
+
+export function getCurrentUser() {
+  const user = localStorage.getItem("user");
+  return user ? JSON.parse(user) : null;
+}
+
+export function logout() {
+  localStorage.removeItem("token");
+  localStorage.removeItem("user");
+}
+
+export function getToken() {
+  return localStorage.getItem("token");
+}
+
+export function getUser() {
+  const user = localStorage.getItem("user");
+  return user ? JSON.parse(user) : null;
+}
+
+export function isAuthenticated() {
+  return !!localStorage.getItem("token");
+}
